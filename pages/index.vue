0cec496a7dab61da1e1d5f833a1fe7d50784e112
dualio26 <eamongreen59@gmail.com>
2021-09-26 00:18:25 +1200

    First Push

---
+++ pages/index.vue
@@ -0,0 +1,266 @@
+<template>
+  <div>
+    <client-only placeholder="loading...">
+      <Header />
+    </client-only>
+    <div class="banner">
+      <div class="scrollable">
+        <img src="../images/Landscape.svg" alt="Landscape" />
+      </div>
+      <div class="SubTitle">
+        <p class="title">Think Global Act Local</p>
+        <p class="buttons">
+          <a href="carbonfootprint.html"
+            ><button style="background-color: #bcd8ee; color: #4a4a4a">
+              <h3>Carbon Footprint</h3>
+            </button></a
+          ><a href="carboncredits.html"
+            ><button style="background-color: #86c886; color: #f5f3ee">
+              <h3>Carbon Credits</h3>
+            </button></a
+          >
+        </p>
+        <p class="heading2">
+          Supporting you in the
+          <span style="color: #89b7db">transition</span> to a
+          <span style="color: #86c886">sustainable</span> future.
+        </p>
+      </div>
+    </div>
+
+    <div class="CaseStudy1">
+      <div class="Capsule1">
+        <div class="content">
+          <div>
+            <img src="../images/Summit.png" alt="WAOSumImage" />
+          </div>
+          <div>
+            <h3>Case Study</h3>
+            <h1 class="CSTitles">WAO Summit</h1>
+            <p class="CSText">
+              Wao is a charitable trust dedicated to educating, inspiring and
+              enabling New Zealand communities to accelerate beyond
+              sustainability towards a thriving future. Based in the Southern
+              Lakes region, WAO look to inspire, educate and enable communities
+              to look towards the opportunities created by this transition and
+              help to shape a future that looks towards not just a sustainable
+              future, but one where people give back more than they take.
+            </p>
+            <a href="waosum.html" class="CSLinks">Read this case study</a>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="CaseStudy2">
+      <img src="../images/WorldBank.png" alt="WorldBankImg" />
+      <h3>Case Study</h3>
+      <h1 class="CSTitles2">
+        Ski Areas Association<br />
+        of New Zealand
+      </h1>
+      <p class="CSText2">
+        Made up of representatives from commercial and club ski fields across
+        New Zealand, SAANZ’s sustainability group, approached EAS well aware of
+        the impacts that climate change can have on the future of the ski and
+        snow-sports industry. An industry so intrinsically linked to weather,
+        nature and the environment.
+      </p>
+      <a href="waosum.html" class="CSLinks">Read this case study</a>
+    </div>
+
+    <div class="CaseStudy3">
+      <img src="../images/WAIWanaka2.png" alt="WorldBankImg" />
+      <h3>Case Study</h3>
+      <h1 class="CSTitles">WAI Wānaka</h1>
+      <p class="CSText3">
+        WAI Wānaka’s vision is to safeguard and enhance water quality, ecosystem
+        function and biodiversity within the Upper Clutha basin. <br /><br />
+        Moving at pace, WAI Wānaka aims to connect the many individuals,
+        community groups, iwi, landowners and businesses undertaking positive
+        work towards building healthy ecosystems and supporting community
+        wellbeing.
+      </p>
+      <a href="waosum.html" class="CSLinks">Read this case study</a>
+    </div>
+
+    <div class="ClientList">
+      <h1>Our Clients</h1>
+      <div class="grid-container">
+        <div class="grid-item"><img src="../images/nz.png" alt="" /></div>
+        <div class="grid-item"><img src="../images/WAO-02.svg" alt="" /></div>
+        <div class="grid-item">
+          <img src="../images/201801_gfoi_logo.png" alt="" />
+        </div>
+        <div class="grid-item"><img src="../images/index.png" alt="" /></div>
+        <div class="grid-item">
+          <img src="../images/ipcc_altlogo_short_rgb.png" alt="" />
+        </div>
+        <div class="grid-item">
+          <img src="../images/worldbankLogo.png" alt="" />
+        </div>
+        <div class="grid-item">
+          <img src="../images/wai-wanaka-logo.png" alt="" />
+        </div>
+        <div class="grid-item">
+          <img src="../images/fao_logo_3lines_en1.png" alt="" />
+        </div>
+      </div>
+    </div>
+
+    <!--This is the div for the services Carousel-->
+    <div class="Services"></div>
+
+    <client-only>
+      <div class="Publications">
+        <h1>Publications</h1>
+        <div class="cards">
+          <div id="C1" class="CColumn">
+            <a href="https://www.reddcompass.org/search-the-mgd">
+              <div class="card1">
+                <h2 class="CTitles">GFOI Methods and Guidance Document</h2>
+                <div class="container">
+                  <p>
+                    Integration of remote-sensing and ground-based observations
+                    for estimation of emissions and removals of greenhouse gases
+                    in forests.
+                  </p>
+                </div>
+              </div>
+            </a>
+
+            <a href="https://doi.org/10.1016/j.rse.2019.111492">
+              <div class="card3">
+                <h2 class="CTitles">
+                  Mitigating the effects of omission errors on area and area
+                  change estimates
+                </h2>
+                <div class="container">
+                  <p>Remote Sensing of Environment</p>
+                </div>
+              </div>
+            </a>
+          </div>
+
+          <div id="C2" class="CColumn">
+            <a
+              href="https://www.ipcc-nggip.iges.or.jp/public/2019rf/index.html"
+            >
+              <div id="card2" class="card2">
+                <h2 class="CTitles">IPCC 2019 Refinement</h2>
+                <div class="container">
+                  <p>
+                    2019 Refinement to the 2006 IPCC Guidelines for National
+                    Greenhouse Gas Inventories
+                  </p>
+                </div>
+              </div>
+            </a>
+
+            <a href="https://doi.org/10.3390/rs13112172">
+              <div id="card4" class="card4">
+                <h2 class="CTitles">
+                  Capacity Development for Use of Remote Sensing for REDD+ MRV
+                </h2>
+                <div class="container">
+                  <p>
+                    Using Online and Offline Activities: Impacts and Lessons
+                    Learned.
+                  </p>
+                </div>
+              </div>
+            </a>
+          </div>
+        </div>
+      </div>
+    </client-only>
+
+    <!--Div for the insight carousel-->
+    <div class="Insights">
+      <CardCarousel />
+    </div>
+
+    <!--Div fo the Who we are carousel-->
+    <div class="WWA"></div>
+
+    <div class="InTouch">
+      <h1>Who we are</h1>
+    </div>
+
+    <footer>
+      <a href="#Top">
+        <i
+          class="fas fa-home fa-2x"
+          style="
+            color: #86c886;
+            float: right;
+            padding-top: 1.25%;
+            padding-right: 2%;
+            top: 0;
+          "
+        ></i>
+      </a>
+      <ul class="flist">
+        <li>
+          <p><a href="tel:0211234123">Phone:</a> 021 1234 123</p>
+        </li>
+
+        <li>
+          <p>
+            <a href="mailto:info@enviroaccounts.com">Email:</a>
+            info@enviroaccounts.com
+          </p>
+        </li>
+      </ul>
+      <ul class="ficons">
+        <li>
+          <a href="mailto:info@enviroaccounts.com"
+            ><i class="fas fa-envelope fa-lg"></i></a
+          >&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;: info@enviroaccounts.com
+        </li>
+        <li>
+          <a href="tel:0211234123"><i class="fas fa-phone fa-lg"></i></a
+          >&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;: 021 1234 123
+        </li>
+      </ul>
+      <p class="fotcrit">
+        © Environmental Accounting Services 2021 | Website by Adam Hall Design
+      </p>
+    </footer>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      polling: null,
+    }
+  },
+  methods: {
+    pollData() {
+      this.polling = setInterval(() => {
+        var height = document.querySelector('#C1').offsetHeight
+        console.log(height + ' column 1 height')
+        var x = height + 'px'
+        document.getElementById('C2').style.height = x
+        const c2h = document.querySelector('#card2').offsetHeight
+        const c4h = document.querySelector('#card4')
+        var style = getComputedStyle(c4h)
+
+        var mb = parseInt(style.marginBottom)
+        console.log(c2h + ' card 2 height')
+        var y = height - c2h - mb * 2
+        console.log(y)
+        document.getElementById('card4').style.height = y + 'px'
+      }, 1000)
+    },
+  },
+  beforeDestroy() {
+    clearInterval(this.polling)
+  },
+  created() {
+    this.pollData()
+  },
+}
+</script>
